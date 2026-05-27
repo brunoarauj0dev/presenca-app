db10a505cb28bb2f9b9f639097ad24fe31aff822
Bruno Araujo <bruninhoaraujo83@gmail.com>
2026-05-26 22:38:45 -0300

    adiciona funcionalidades e melhorias visuais

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const botao = document.querySelector("button");
 
+const input = document.querySelector("input");
+
+const lista = document.querySelector("#lista");
+
+const contador = document.querySelector("#contador");
+
+function atualizarContador() {
+    contador.textContent = "Presentes: " + lista.children.length;
+}
+
 botao.addEventListener("click", function() {
 
-    console.log("clicou");
+    console.log(input.value);
+    if (input.value === "") {
+        return;
+    }
+    const item = document.createElement("li");
+    const botaoremover = document.createElement("button");
+        botaoremover.classList.add("remover");
+    botaoremover.textContent = "Remover";
+        botaoremover.addEventListener("click", function() {
+            item.remove();
+            atualizarContador();
+        });
+    item.textContent = input.value;
+        item.appendChild(botaoremover);
+    lista.appendChild(item);
+        atualizarContador();
+    input.value = "";
+
+});
+
+input.addEventListener("keydown", function(event) {
 
 });
